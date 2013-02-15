a6d031d1256e3cef5c8c122df7024a807752fb09
maisaengineering <contact@maisasolutions.com>
2013-02-15 10:18:42 +0530

    csv export added to company using comma

--- app/models/company.rb
+++ app/models/company.rb
@@ -17,38 +17,53 @@
 #
 
 class Company < ActiveRecord::Base
-	# Using ThinkingSphinx now... (Adam - 2009-06-18)
-    # acts_as_ferret :fields => [:name]
-    acts_as_log_edits
+  # Using ThinkingSphinx now... (Adam - 2009-06-18)
+  # acts_as_ferret :fields => [:name]
+  acts_as_log_edits
 
-    extend Listable::ModelHelper
+  extend Listable::ModelHelper
 
-    has_many :employees, :foreign_key => "company_id"
+  has_many :employees, :foreign_key => "company_id"
 
-	# Thinking Sphinx Config
-	define_index do
+  # Thinking Sphinx Config
+  define_index do
 
-		indexes :name
-		set_property :delta => true				
-	end
+    indexes :name
+    set_property :delta => true
+  end
 
-    validates_uniqueness_of :name
-    validates_presence_of :name
+  validates_uniqueness_of :name
+  validates_presence_of :name
 
-    PERPAGE = 100
-  
-    def self.list_companies(list_options = {})
-        self.find(:all, :order => "name").map do |c|
-            [c.name, (list_options[:name] ? c.name : c.id)]
-        end.unshift(["", nil])
-    end    
+  PERPAGE = 100
 
-    # Method for autocompletes that will list out all employee names that are similar to the 'query' parameter
-    # If use_id is set to true, the results will be returned like this ("Hugh Bien (id)")  
-    def self.list_companies_for_autocomplete(query, use_id=false)
-        self.find(:all, :conditions => ["name LIKE ?", '%' + query + '%'],  :order => "name ASC", :limit => 20).map do |c| 
-            c.name
-        end
-    end    
-  
+  def self.list_companies(list_options = {})
+    self.find(:all, :order => "name").map do |c|
+      [c.name, (list_options[:name] ? c.name : c.id)]
+    end.unshift(["", nil])
+  end
+
+  # Method for autocompletes that will list out all employee names that are similar to the 'query' parameter
+  # If use_id is set to true, the results will be returned like this ("Hugh Bien (id)")
+  def self.list_companies_for_autocomplete(query, use_id=false)
+    self.find(:all, :conditions => ["name LIKE ?", '%' + query + '%'],  :order => "name ASC", :limit => 20).map do |c|
+      c.name
+    end
+  end
+
+
+  # ===============
+  # = CSV support =
+  # ===============
+  comma do
+    Company.column_names.each do |n|
+      if n.eql?('updated_at')
+        send(n){|n| n.strftime('%m/%d/%Y %H:%M %p') }
+      else
+        send(n) unless n.eql?('delta')
+      end
+    end
+  end
+
+
 end
