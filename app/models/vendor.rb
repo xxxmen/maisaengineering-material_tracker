7c4a4a492fb349ce222d035deab82147b9058656
maisaengineering <contact@maisasolutions.com>
2013-02-16 11:16:09 +0530

    pulling code

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -25,112 +25,112 @@
 
 require 'digest/sha1'
 
-class Vendor < ActiveRecord::Base  
-	############################################################################
-	# MIXINS
-	############################################################################
+class Vendor < ActiveRecord::Base
+  ############################################################################
+  # MIXINS
+  ############################################################################
 
-  	extend Listable::ModelHelper
-  	
-	############################################################################
-	# CONSTANTS
-	############################################################################
+  extend Listable::ModelHelper
 
-	PERPAGE = 100
+  ############################################################################
+  # CONSTANTS
+  ############################################################################
 
-	############################################################################
-	# ASSOCIATIONS
-	############################################################################
-	
-  	acts_as_log_edits  
-  	has_many :orders
-    has_and_belongs_to_many :vendor_groups
+  PERPAGE = 100
 
-	############################################################################
-	# INDEXING
-	############################################################################
-  	
-	# Thinking Sphinx Config
-	define_index do
-		indexes :vendor_no
-		indexes :name
-		indexes :account_no
-		set_property :delta => true
-	end
-	
-	############################################################################
-	# CALLBACKS
-	############################################################################
-  
-  	before_create :set_access_key
-  
-  	############################################################################
-	# VALIDATIONS
-	############################################################################
+  ############################################################################
+  # ASSOCIATIONS
+  ############################################################################
 
-  	validates_presence_of :name
-  
-  
-  	############################################################################
-	# INSTANCE METHODS
-	############################################################################
+  acts_as_log_edits
+  has_many :orders
+  has_and_belongs_to_many :vendor_groups
 
-  
-	def self.all_vendors(header = '-- Vendors --')
-		options = self.find(:all, :order => "name ASC").map do |v|
-		    [v.name, v.id]
-		end
-		if header
-			options.unshift([header, nil])
-		end
-		return options
-	end   
-  
-	def self.list_vendors_by_name
-		self.find(:all, :order => "name ASC").map do |v|
-		    [v.name, v.name]
-		end.unshift([nil, nil])
-	end  
-  
-	def self.options_for_overdue
-		vendors = Order.find(:all, :select => "DISTINCT(vendor_id)", :conditions => 
-		    ["purchase_orders.date_eta IS NOT NULL AND 
+  ############################################################################
+  # INDEXING
+  ############################################################################
+
+  # Thinking Sphinx Config
+  define_index do
+    indexes :vendor_no
+    indexes :name
+    indexes :account_no
+    set_property :delta => true
+  end
+
+  ############################################################################
+  # CALLBACKS
+  ############################################################################
+
+  before_create :set_access_key
+
+  ############################################################################
+  # VALIDATIONS
+  ############################################################################
+
+  validates_presence_of :name
+
+
+  ############################################################################
+  # INSTANCE METHODS
+  ############################################################################
+
+
+  def self.all_vendors(header = '-- Vendors --')
+    options = self.find(:all, :order => "name ASC").map do |v|
+      [v.name, v.id]
+    end
+    if header
+      options.unshift([header, nil])
+    end
+    return options
+  end
+
+  def self.list_vendors_by_name
+    self.find(:all, :order => "name ASC").map do |v|
+      [v.name, v.name]
+    end.unshift([nil, nil])
+  end
+
+  def self.options_for_overdue
+    vendors = Order.find(:all, :select => "DISTINCT(vendor_id)", :conditions =>
+        ["purchase_orders.date_eta IS NOT NULL AND
 		    purchase_orders.completed = ? AND 
 		    po_statuses.status <> ? AND 
 		    purchase_orders.date_eta <= CURDATE() AND
-		    purchase_orders.vendor_id IS NOT NULL", false, 'INACTIVE'], 
-		    :include => [:status, :vendor], :order => "vendors.name")
-		vendors = vendors.delete_if { |o| o.blank? || o.vendor.blank? }
-		vendors = vendors.map do |order| 
-		    [order.vendor.name, order.vendor.name] 
-		end.uniq
-		vendors = vendors.unshift(["UNSPECIFIED", "UNSPECIFIED"]).unshift(['ALL', ''])
-		return vendors
-	end
-  
-  	##
-  	#	Finds the vendor that matches the passed in value *name* and 
-  	#	returns the telephone number for it if it finds one.
-  	#
-  	def self.find_vendors_telephone(name = "")
-  		phone = ""
-  		unless name.blank?
-	  		query = "%#{name}%"
-	  		vendor = self.find(:first, :conditions => ["name LIKE ?", query])
-	  		if vendor && !vendor.telephone.blank?
-	  			phone = vendor.telephone
-			end
-		end
-		return phone
- 	end
-  
-   	############################################################################
-	# PRIVATE
-	############################################################################
+		    purchase_orders.vendor_id IS NOT NULL", false, 'INACTIVE'],
+                         :include => [:status, :vendor], :order => "vendors.name")
+    vendors = vendors.delete_if { |o| o.blank? || o.vendor.blank? }
+    vendors = vendors.map do |order|
+      [order.vendor.name, order.vendor.name]
+    end.uniq
+    vendors = vendors.unshift(["UNSPECIFIED", "UNSPECIFIED"]).unshift(['ALL', ''])
+    return vendors
+  end
 
-  	private
-  
-  	def set_access_key
-    	self.access_key = Digest::SHA1.hexdigest('--' + self.id.to_s + '--' + Time.now.to_s + '--')
-  	end
+  ##
+  #	Finds the vendor that matches the passed in value *name* and
+  #	returns the telephone number for it if it finds one.
+  #
+  def self.find_vendors_telephone(name = "")
+    phone = ""
+    unless name.blank?
+      query = "%#{name}%"
+      vendor = self.find(:first, :conditions => ["name LIKE ?", query])
+      if vendor && !vendor.telephone.blank?
+        phone = vendor.telephone
+      end
+    end
+    return phone
+  end
+
+  ############################################################################
+  # PRIVATE
+  ############################################################################
+
+  private
+
+  def set_access_key
+    self.access_key = Digest::SHA1.hexdigest('--' + self.id.to_s + '--' + Time.now.to_s + '--')
+  end
 end
